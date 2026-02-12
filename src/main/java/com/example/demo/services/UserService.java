cc966f424c1bec176c5a2e7ab81cf571758f2fb0
Gustavo Gonzaga <gustavogonzaga1225@gmail.com>
2026-02-12 15:06:08 -0300

    Handling update

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -4,6 +4,7 @@ import com.example.demo.entities.User;
 import com.example.demo.repositories.UserRepository;
 import com.example.demo.services.exceptions2.DataBaseException;
 import com.example.demo.services.exceptions2.ResourcedNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -44,9 +45,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj){
-        User entity = repository.getReferenceById(id);
-        updateData(entity,obj);
-        return repository.save(entity);
+        try{
+            User entity = repository.getReferenceById(id);
+            updateData(entity,obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e ){
+            throw  new ResourcedNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
